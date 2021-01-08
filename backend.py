2023a1ddd58d8cf2cbd31b4599da7ee666571922
Japhei <62942290+Japhei@users.noreply.github.com>
2021-01-08 12:01:50 +0100

    Live
    asdas

--- backend.py
+++ backend.py
@@ -1,66 +1,44 @@
-#definitions
-global table, state, pointer, band, emptyBand
-table =[["1", "#", "#", "R", "1"], 
-        ["1", "B", "B", "R", "2"], 
-        ["2", "B", "#", "R", "3"], 
-        ["2", "#", "#", "L", "6"], 
-        ["3", "B", "#", "R", "4"], 
-        ["3", "#", "#", "L", "7"], 
-        ["4", "B", "B", "R", "2"], 
-        ["4", "#", "#", "L", "7"], 
-        ["5", "#", "#", "L", "6"], 
-        ["6", "B", "B", "L", "7"], 
-        ["6", "#", "#", "R", "E"], 
-        ["7", "#", "#", "L", "5"]]
+table = ([  ["1", "B", "B", "R", "2"], #überspringe den erstn Baum
+            ["2", "B", "B", "R", "3"], #überspringe den zweiten Baum
+            ["3", "B", "#", "R", "1"], #lösche den dritten Baum
+         ])
 state = "1"
 pointer = 0
-band = ["#", "B", "B", "B", "B", "B", "B", "B", "B", "B", "B", "B", "B", "B", "B", "B", "B", "B", "B", "B", "B", "B", "B", "B", "B", "B", "B", "B", "B", "B", "B", "B", "B", "B", "B", "#"]
-emptyBand = "#"
+band = []
 
-class Backend:
+def fillBand(length):
+    global band
+    for i in range (0, length): #geht band einmal durch
+        band.append("B")    #band wird für jedes i um "B" erweitert
 
-    class BackendException(Exception): #die Basis für alle Buntzerdefinierten Exceptions
-        def getErrorMessage(self):
-            pass
+def appendLeft():  
+    global band
+    band.insert(0, "#") #das band wird links um eine stelle erweitert
+    pointer += 1
 
-    class StateNotFoundException(BackendException): #wenn eine Zustand nicht übereinstimmt
-        pass
-
-    class BandOutOfBoundsException(BackendException): #wenn band nicht reicht
-        def getErrorMessage(self):
-            return "ERROR: band out of bounds"
-
-    class ObjectNotFoundException(BackendException):    #wenn ein Objekt nicht gefunden wird
-        pass
-        
-    def nextStep(self):
-        global table, band, pointer, state
-        for column in table: #gehe jede spalte durch
-            if column[0] == state:   #wenn der aktuelle zustand stimmt
-                if len(band) > pointer:
-                    if column[1] == band[pointer]: #wenn man das richtige liest
-                        band[pointer] = column[2] #ersetzte mit dem schreibe element
-                        pointer += 1 if column[3] == "R" else -1
-                        state = column[4]
-                        return True
-                    continue
-                raise self.BandOutOfBoundsException()
-
-        return False
-
+def appendRight():
+    global band
+    band.append("#")    #das band wird rehts um eine stelle erweitert  
     
+def nextStep():
+    global table, state, pointer, band
+    for column in table: #gehe jede spalte durch
+        if column[0] == state:   #wenn der aktuelle zustand stimmt
+            if column[1] == band[pointer]: #wenn man das richtige liest
+                band[pointer] = column[2]  #ersetzte mit dem schreibe element
+                change = 1 if column[3] == "R" else - 1
+                pointer += change  #ändere den Pointer entsprechend der richtung
+                if change == 1: appendRight()
+                else: appendRight()
+                state = column[4] #setzte den neuen state
+                return True
+    return False
 
-
+            
+fillBand(10)  #band wird gefüllt
+print(band)
 try:
-    backend = Backend()
-    print("state: " + state + " position: " + str(pointer) + " band: " + str(band))
-    while backend.nextStep():
-        print("state: " + state + " position: " + str(pointer) + " band: " + str(band))
-except Backend.BackendException() as e:
-    print(e.getErrorMessage())
-
-
-""" Konsole: 
-The host doesn’t allow starting the debugger. If needed, ask them to enable it.
-
-"""
+    while nextStep():
+        print(band)
+finally:
+    print("Done")
