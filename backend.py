93ee8d909b77588a21ad722930fb34e8cf0f14d9
julian-iso <julian.maurus2307@gmail.com>
2021-01-09 17:46:28 +0100

    test

---
+++ backend.py
@@ -0,0 +1,44 @@
+table = ([  ["1", "B", "B", "R", "2"], #überspringe den erstn Baum
+            ["2", "B", "B", "R", "3"], #überspringe den zweiten Baum
+            ["3", "B", "#", "R", "1"], #lösche den dritten Baum
+         ])
+state = "1"
+pointer = 2
+band = []
+
+def fillBand(length):
+    global band
+    for i in range (0, length): #geht band einmal durch
+        band.append("B")    #band wird für jedes i um "B" erweitert
+
+def appendLeft():  
+    global band
+    band.insert(0, "#") #das band wird links um eine stelle erweitert
+    pointer += 1
+
+def appendRight():
+    global band
+    band.append("#")    #das band wird rehts um eine stelle erweitert  
+    
+def nextStep():
+    global table, state, pointer, band
+    for column in table: #gehe jede spalte durch
+        if column[0] == state:   #wenn der aktuelle zustand stimmt
+            if column[1] == band[pointer]: #wenn man das richtige liest
+                band[pointer] = column[2]  #ersetzte mit dem schreibe element
+                change = 1 if column[3] == "R" else - 1
+                pointer += change  #ändere den Pointer entsprechend der richtung
+                if change == 1: appendRight()
+                else: appendRight()
+                state = column[4] #setzte den neuen state
+                return True
+    return False
+
+            
+fillBand(10)  #band wird gefüllt
+print(band)
+try:
+    while nextStep():
+        print(band)
+finally:
+    print("Done")
