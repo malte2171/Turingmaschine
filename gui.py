2baf82b1c17115de588734cd46db1cd6134cb5b5
Japhei <62942290+Japhei@users.noreply.github.com>
2021-01-31 18:55:22 +0100

    debugging changes for teams

--- gui.py
+++ gui.py
@@ -27,7 +27,13 @@ pointer = 0
  
 global stateIndex
 stateIndex = 0
+
+global graphics
+
+global root
  
+global running
+running = False
 
 #------------------------------
 # init
@@ -41,18 +47,27 @@ fillBand(50)   #band gets filled with 50 elements
 
    
 
-def run(): #the running cirlce   
-   print("next run") 
-   graphics.mapBand()
-   graphics.mapTable()
-   dumpMap()
-   b = nextStep()
-   graphics.update()
-   if (b):
-      root.after(2500, func=run)
-   else:
-      print("DONE")
+def run(): #the running cirlce 
+    if not running:
+        return
+    print("next run") 
+    graphics.mapBand()
+    graphics.mapTable()
+    dumpMap()
+    b = nextStep()
+    graphics.update()
+    if (b):
+        root.after(2500, func=run)
+    else:
+        print("DONE")
 
+def reset():
+    global graphics, root
+    print("reset")
+    root.destroy()
+    root = tk.Tk()
+    graphics = GUI(root)
+    root.mainloop()
  
 class GUI(tk.Frame):
  
@@ -64,19 +79,19 @@ class GUI(tk.Frame):
         parent.configure(bg="#36393F")
  
         # StartButton
-        bandLeft = tk.Button(parent, fg="black", bg="#43B581", font=50, text="Start", command=self.start)
-        bandLeft.place(x=50, y=125, width=250, height=50)
+        self.startButton = tk.Button(parent, fg="black", bg="#43B581", font=50, text="Start", command=self.start)
+        self.startButton.place(x=50, y=125, width=250, height=50)
  
-        # Pointer
-        self.pointer = tk.Label(parent, bg="#F04747", font=50, text="V")
+        # Pointer for band
+        self.pointer = tk.Button(parent, bg="#F04747", font=50, text="V", command=self.moveCenter)
         self.pointer.place(x=50 - bandIndex, y=0, width=50, height=25)  # index angezeigter Feld - links rechts
  
-        # Second Pointer
-        self.secondPointer = tk.Label(parent, bg="#F04747", font=50, text="Ʌ")
+        # Second Pointer for band
+        self.secondPointer = tk.Button(parent, bg="#F04747", font=50, text="Ʌ", command=self.moveCenter)
         self.secondPointer.place(x=50, y=75, width=50, height=25)
  
-        # state Pointer
-        self.statePointer = tk.Label(parent, bg="#FAA61A", font=50, text="V")
+        # state pointer for table
+        self.statePointer = tk.Button(parent, bg="#FAA61A", font=50, text="V", command=self.centerTable)
  
         # Band
         bandLeft = tk.Button(parent, fg="#ffffff", bg="#2F3136", font=50, text="<", command=self.bandBack)
@@ -104,9 +119,8 @@ class GUI(tk.Frame):
         self.addCol.place(x = 300, y = 125, width = 125, height = 50)
         self.add()
         
-        self.removeCol = tk.Button(parent, bg="gray", font= 50, text = "Remove", command=self.remove)
-        self.removeCol.place( x = 425, y = 125, width = 125, height = 50)
-        self.remove()
+        self.reset = tk.Button(parent, bg="gray", font= 50, text = "Reset", command=self.reset)
+        self.reset.place( x = 425, y = 125, width = 125, height = 50)
 
         tableLeft = tk.Button(parent,fg="#ffffff", bg="#2F3136", font= 60, text="<", command=self.tableLeft)
         tableLeft.place(x = 0, y = 225, width = 50, height = 250)
@@ -115,64 +129,58 @@ class GUI(tk.Frame):
         tableRight.place(x = 550, y = 225, width = 50, height = 250)
 
         self.update()
-        
-        #Error Label
-
-        self.ErrorLabel = tk.Label(parent,  fg="#000000", bg="#ffffff", font= 60, text="Error: du bist gay")
-        self.ErrorLabel.place(x = 0, y=500, width = 600, height = 100)
-        
-        self.errorButton = tk.Button(parent,fg="#ffffff", bg="#2F3136", font= 60, text="change Label", command=self.error)
-        self.errorButton.place(x = 0, y=610, width = 600, height = 100)
-
-    def error(self, errorMessage=""):
-        print(errorMessage)
-        self.ErrorLabel.config(text=errorMessage)  
-      
+        self.updateTable()
             
     def update(self):
-        global band, bandIndex, stateIndex
+        global band, bandIndex, stateIndex, tableIndex, running
         for i in range(10):
             self.arr[i].delete(0, tk.END)
             self.arr[i].insert(0, band[i + bandIndex])
         a = 50 - 50 * bandIndex + 50 * pointer
         c=bandIndex+pointer
-        if (a >= 0 and a <= 550):
-            self.pointer['text'] = "V"
-            self.pointer.place(x=a, y=0, width=50, height=25)  # index angezeigter Feld - links rechts
-            self.secondPointer.place(x=a, y=75, width=50, height=25)
+        if (a < 0):
+            a = 0
+        elif (a > 550):
+            a = 550
+        self.pointer['text'] = "V"
+        self.pointer.place(x=a, y=0, width=50, height=25)  # index angezeigter Feld - links rechts
+        self.secondPointer.place(x=a, y=75, width=50, height=25)
         if(c > 0):
             self.pointer['text'] = str(-c)
         elif(c < -9):
             self.pointer['text'] = str(c+9)
         else:
             self.pointer['text'] = "V"
- 
-        b = 100 + stateIndex * 50
-        self.statePointer.place(x=b, y=200, width=50, height=25)
+        if (running):
+            self.startButton["text"] = "Stop"
+        else:
+            self.startButton["text"] = "Start"
  
    
     def start(self):
-        print("start")
-        run()
+        global running
+        print("start/stop")
+        running = not running
+        if (running):
+            run()
   
     def add(self):
         global tableLength
         tableLength += 1
-        table.append([])
+        print(tableLength)
+        print("table length += 1")
+        table.append(["", "", "", "", ""])
+        print("table was appended")
         if (tableLength < 10):
+            print("table length is under 10")
             tableElement = []
             for j in range(5):
                 element = tk.Entry(self.parent)
                 element.place(x=tableLength * 50 + 50, y=j * 50 + 225, width=50, height=50)
                 tableElement.append(element)
             self.tableElements.append(tableElement)
-
-    def remove(self):
-        global tableLength
-        if (tableLength > 1):
-            tableLength = tableLength - 1
-            del self.tableElements[-1]                   #ändern zum namen der Tabelle wenn es soweit ist
-         
+            print("created new column")
+    
     def bandForward(self):        #index + 1 -->  bewegt nach rechts
         global bandIndex
         self.mapBand()
@@ -184,6 +192,13 @@ class GUI(tk.Frame):
         self.mapBand()
         bandIndex -= 1
         self.update()
+    
+    def moveCenter(self):
+        global bandIndex
+        bandIndex = -2
+        print("moved bandindex to center")
+        print(bandIndex)
+        self.update()
 
     def tableLeft(self):
         global table, tableIndex, tableLength
@@ -193,12 +208,11 @@ class GUI(tk.Frame):
             self.updateTable()
 
     def tableRight(self):
-        global table, tableIndex
-        self.mapTable()
-        for x in range(10 if tableIndex >= 10 else tableIndex + 1):
-            for y in range(5):
-                self.tableElements[x][y].delete()
-                self.tableElements[x][y].insert(0, table[x - tableIndex][y])
+        global table, tableIndex, tableLength
+        if tableLength >= 10 and tableIndex > 9-tableLength:
+            self.mapTable()
+            tableIndex -= 1
+            self.updateTable()
             
     def mapTable(self):
         global table, tableLength, tableIndex
@@ -213,23 +227,48 @@ class GUI(tk.Frame):
                 print(" x: " + str(x) + " y: " + str(y))
                 column.append(self.tableElements[x][y].get())
             table[x - tableIndex] = column
- 
+
     def updateTable(self):
-        global table, tableLength
+        global table, tableLength, tableIndex
         print("update table")
         print("tableIndex: " + str(tableIndex))
         print("tableLength: " + str(tableLength))
-        for x in range(8 if tableLength >= 8 else tableLength):
+        for x in range(9 if tableLength >= 9 else tableLength):
             for y in range(5):
                 print(" x: " + str(x) + " y: " + str(y))
                 self.tableElements[x][y].delete(0, tk.END)
-                self.tableElements[x][y].insert(0, table[x - tableIndex][y])
+                self.tableElements[x][y].insert(0, table[x - tableIndex][y])       
+        b = 100 + stateIndex * 50 + tableIndex * 50
+        if (b < 50):
+            b = 0
+        elif (b > 550):
+            b = 550
+        print("B:" + str(b))
+        self.statePointer.place(x=b, y=200, width=50, height=25)
+
+    def centerTable(self):
+        global tableIndex
+        tableIndex = 0
+        self.updateTable()
 
     def mapBand(self): #liest Eingabe von Band aus und fügt sie in ein Array ein
         global band, bandIndex
         for i in range(10):
             value = self.arr[i].get()
             band[bandIndex + i] =  value if len(value) > 0 else "#"
+
+    def reset(self):
+        global bandIndex, band, tableIndex, table, tableLength, state, pointer, stateIndex
+        bandIndex = 0
+        band = []
+        fillBand(10)
+        tableIndex = 0
+        table = []
+        tableLength = 0
+        state = 1
+        pointer = 0
+        stateIndex = 0
+        reset()
 
  
 #deprecated for debug reasons only
@@ -286,7 +325,7 @@ def nextStep():
 #------------------------------
 # Programm ausführen
 #------------------------------
-  
+
 root = tk.Tk()
 graphics = GUI(root)
 root.mainloop()
