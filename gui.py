ed9c5bcecd882168805961824700740b0320e3c6
Japhei <62942290+Japhei@users.noreply.github.com>
2021-02-04 00:00:26 +0100

    Group G: Error managing
    Fehler bei der Nutzereingabe werden erkannt und dem Nutzer kenntlich gemacht.

--- gui.py
+++ gui.py
@@ -4,6 +4,7 @@ import tkinter as tk
 # GLOBAL variables
 #------------------------------
 
+
 global band
 band = []
  
@@ -38,6 +39,15 @@ running = False
 #------------------------------
 # init
 #------------------------------
+def popup(msg):
+    popup = tk.Tk()
+    popup.title("Error Message")
+    label = tk.Label(popup, text=msg)
+    label.pack(side="top", fill="x", pady=10)
+    B1 = tk.Button(popup, text="Okay", command = popup.destroy)
+    B1.pack()
+    popup.mainloop()
+
 def fillBand(length):   #band gets filled
     global band
     for i in range(0, length):  
@@ -47,19 +57,25 @@ fillBand(50)   #band gets filled with 50 elements
 
    
 
-def run(): #the running cirlce 
-    if not running:
+def run(): #the running cirlce
+    global running
+    if (not running):
         return
-    print("next run") 
-    graphics.mapBand()
-    graphics.mapTable()
+    print("next run")
     dumpMap()
     b = nextStep()
     graphics.update()
+    graphics.updateTable()
     if (b):
+        if (state==graphics.finalState.get()):
+            print("DONE")
+            popup("DONE!\nReached final state.")
+            return
         root.after(2500, func=run)
     else:
+        running = False
         print("DONE")
+        popup("DONE!\nAn error occured.")
 
 def reset():
     global graphics, root
@@ -96,7 +112,10 @@ class GUI(tk.Frame):
         # Band
         bandLeft = tk.Button(parent, fg="#ffffff", bg="#2F3136", font=50, text="<", command=self.bandBack)
         bandLeft.place(x=0, y=25, width=50, height=50)
- 
+        
+        self.highlightBand = []
+        self.highlightTable = []
+
         self.arr = []
         for i in range(10):
             element = tk.Entry(parent)
@@ -121,6 +140,11 @@ class GUI(tk.Frame):
         
         self.reset = tk.Button(parent, bg="gray", font= 50, text = "Reset", command=self.reset)
         self.reset.place( x = 425, y = 125, width = 125, height = 50)
+        
+        
+        self.finalState = tk.Entry(parent)
+        self.finalState.place(x = 550, y = 125, width = 50, height = 50)
+        self.finalState.insert(0, "E")
 
         tableLeft = tk.Button(parent,fg="#ffffff", bg="#2F3136", font= 60, text="<", command=self.tableLeft)
         tableLeft.place(x = 0, y = 225, width = 50, height = 250)
@@ -128,14 +152,27 @@ class GUI(tk.Frame):
         tableRight = tk.Button(parent,fg="#ffffff", bg="#2F3136", font= 60, text=">", command=self.tableRight)
         tableRight.place(x = 550, y = 225, width = 50, height = 250)
 
+
         self.update()
         self.updateTable()
             
     def update(self):
         global band, bandIndex, stateIndex, tableIndex, running
+        print("start upsate: " + str(self.highlightBand) + " " + str(len(self.highlightBand)))
         for i in range(10):
-            self.arr[i].delete(0, tk.END)
-            self.arr[i].insert(0, band[i + bandIndex])
+            print("i: " + str(i))
+            try:
+                self.arr[i].delete(0, tk.END)
+                self.arr[i].insert(0, band[i + bandIndex])
+                self.arr[i].config(bg="#ffffff")
+            except:
+                print("Ooops, an error occured. Please dont ask the developers. xD") #I really don't know what happens.
+            
+            for h in self.highlightBand:
+                if h == i + bandIndex:
+                    self.arr[i].config(bg="#ff8888")
+            
+
         a = 50 - 50 * bandIndex + 50 * pointer
         c=bandIndex+pointer
         if (a < 0):
@@ -159,10 +196,14 @@ class GUI(tk.Frame):
    
     def start(self):
         global running
-        print("start/stop")
-        running = not running
-        if (running):
-            run()
+        self.highlightBand = []
+        self.highlightTable = []        
+        self.mapBand()
+        self.mapTable()
+        if self.checkInput():
+            running = not running
+            if (running):
+                run()
   
     def add(self):
         global tableLength
@@ -237,7 +278,12 @@ class GUI(tk.Frame):
             for y in range(5):
                 print(" x: " + str(x) + " y: " + str(y))
                 self.tableElements[x][y].delete(0, tk.END)
-                self.tableElements[x][y].insert(0, table[x - tableIndex][y])       
+                self.tableElements[x][y].insert(0, table[x - tableIndex][y])
+                self.tableElements[x][y].config(bg="#ffffff")
+                
+                for h in self.highlightTable:
+                    if h[0] == x - tableIndex and h[1] == y:
+                        self.tableElements[x][y].config(bg="#ff8888")
         b = 100 + stateIndex * 50 + tableIndex * 50
         if (b < 50):
             b = 0
@@ -255,7 +301,8 @@ class GUI(tk.Frame):
         global band, bandIndex
         for i in range(10):
             value = self.arr[i].get()
-            band[bandIndex + i] =  value if len(value) > 0 else "#"
+            #band[bandIndex + i] =  value if len(value) > 0 else "#" # wenn man zu schlau für seinen eigenen code ist! ;-{
+            band[bandIndex + i] = value    
 
     def reset(self):
         global bandIndex, band, tableIndex, table, tableLength, state, pointer, stateIndex
@@ -269,8 +316,70 @@ class GUI(tk.Frame):
         pointer = 0
         stateIndex = 0
         reset()
-
  
+    def checkInput(self):
+        global table, tableLength, band
+        errorMessage = ""
+        for i in range(len(band)):
+            string = band[i]
+            if len(string) != 1:
+                errorMessage += "[Band][" + str(i + 1) + "] Wrong syntax. Please use a length of 1.\n"
+                self.highlightBand.append(i)
+        
+        for x in range(len(table)):
+            for y in range(5):
+                string2 = table[x][y]
+                if len(string2) != 1:
+                    errorMessage += "[Table][" + str(x + 1) + "][" + str(y + 1) + "]  Wrong syntax. Please use a length of 1.\n"
+                    self.highlightTable.append([x, y])
+
+        states = []  #all existing states
+
+        def stateExists(value):
+            for state in states:
+                if (value == state):
+                    return True
+            return False
+
+        if "E" in states:
+            final = True 
+                
+        for x in range(tableLength): #add exisiting states
+            if (not stateExists(table[x][0])):
+                states.append(table[x][0])
+        
+        for x in range(tableLength): #check if nextStep exists
+            nextState = table[x][4]
+            if (not stateExists(nextState) and nextState != self.finalState.get()):
+                errorMessage += "[Table][" + str(x + 1) + "][5] The state '" + nextState + "' is not defined! Please assign.\n"
+                self.highlightTable.append([x, 4])
+
+        for x in range(tableLength): #check direction
+            if not (str(table[x][3]).lower() == "r" or str(table[x][3]).lower() == "l"):
+                errorMessage += "[Table][" + str(x + 1) + "][4] Direction '" + table[x][3] + "' is not defined! Use R/L only.\n"
+                self.highlightTable.append([x, 3])
+
+        if (len(self.finalState.get()) != 1): #check if final state is defined
+            self.finalState.config(bg="#ff8888")
+        else:
+            self.finalState.config(bg="#ffffff")
+            
+        for x in range(tableLength): #check for end state as normal state
+            if (table[x][0] == self.finalState.get()):
+                errorMessage += "[Table][" + str(x + 1) + "][1] Final state '" + table[x][3] + "' can't be defined as state! Use this to end the program.\n"
+                self.highlightTable.append([x, 0])
+
+        if (errorMessage != ""):
+            self.update()
+            self.updateTable()
+            popup(errorMessage)
+        print("---finished check")
+        return errorMessage == ""
+    
+
+
+
+
 #deprecated for debug reasons only
 def dumpMap():
    print("tableLength: " + str(len(table)))
